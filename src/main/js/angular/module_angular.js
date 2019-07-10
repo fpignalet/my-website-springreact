2a6d43bba1f70683fbc5857b5f5d6b4e993b0e6e
francois pignalet <francois.pignalet@gmail.com>
2019-07-10 14:55:11 +0200

    Angular module cleaned up

--- src/main/js/angular/module_angular.js
+++ src/main/js/angular/module_angular.js
@@ -6,19 +6,11 @@
 /*************************************************************************************
  * IMPLEMENTATION
  *************************************************************************************/
-class AngularTests {
+class TestApp1 {
 
-    /// @brief add angularjs to tech tests
-    /// @param name desc...
-    /// @param ctrl desc...
-    /// @param ifirstName desc...
-    /// @param ilastName desc...
-    /// @param products desc...
-    testANGULAR1(contener, id, ifirstName, ilastName, products) {
-        const appname = contener.getElementById("angutest1").getAttribute("ng-app");
-        const ctrlname = contener.getElementById("angutest1").getAttribute("ng-controller");
-
+    create(appname, ctrlname, ifirstName, ilastName, products) {
         const testapp = angular.module(appname, []);
+
         testapp.controller(ctrlname, ["$scope", function($scope) {
 
             // -------------------------------------
@@ -57,6 +49,58 @@ class AngularTests {
             };
 
         }]);
+    }
+}
+
+class TestApp2 {
+
+    create(appname, ctrlname) {
+        const testapp = angular.module(appname, [ 'ngRoute' ]);
+
+        testapp.config(["$routeProvider", function($routeProvider) {
+
+            $routeProvider
+                .when("/Menu1", {
+                    template : () => {
+                        return "<h1>Choosed: Menu 1</h1><p>result = 75%.</p>"
+                    },
+                    controller: ctrlname
+                })
+                .when("/Menu2", {
+                    template : () => {
+                        return "<h1>Choosed: Menu 2</h1><p>result = 95%.</p>"
+                    },
+                    controller: ctrlname
+                })
+                .otherwise({
+                    template : () => {
+                        return "<h1>Choosed: Nothing</h1><p>Nothing has been selected</p>"
+                    },
+                    controller: ctrlname
+                });
+        }]);
+
+        testapp.controller(ctrlname, ["$scope", function($scope) {
+            console.log("$scope = " + $scope.toString());
+        }]);
+    }
+
+}
+
+class AngularTests {
+
+    /// @brief add angularjs to tech tests
+    /// @param name desc...
+    /// @param ctrl desc...
+    /// @param ifirstName desc...
+    /// @param ilastName desc...
+    /// @param products desc...
+    testANGULAR1(contener, id, ifirstName, ilastName, products) {
+        const appname = contener.getElementById("angutest1").getAttribute("ng-app");
+        const ctrlname = contener.getElementById("angutest1").getAttribute("ng-controller");
+
+        const app1 = new TestApp1();
+        app1.create(appname, ctrlname, ifirstName, ilastName, products);
 
 //        console.log(this.getFuncName() + "OK");
     }
@@ -68,39 +112,11 @@ class AngularTests {
     /// @param ilastName desc...
     /// @param products desc...
     testANGULAR2(contener, id) {
-        function onmenu1() {
-            return "<h1>Choosed: Menu 1</h1><p>result = 75%.</p>"
-        }
-        function onmenu2() {
-            return "<h1>Choosed: Menu 2</h1><p>result = 95%.</p>"
-        }
-        function ondefault() {
-            return "<h1>Choosed: Nothing</h1><p>Nothing has been selected</p>"
-        }
-
         const appname = contener.getElementById(id).getAttribute("ng-app");
         const ctrlname = contener.getElementById(id).getAttribute("ng-controller");
 
-        const otherapp = angular.module(appname, [ 'ngRoute' ]);
-        otherapp.config(["$routeProvider", function($routeProvider) {
-
-            $routeProvider
-                .when("/Menu1", {
-                    template : onmenu1(),
-                    controller: ctrlname
-                })
-                .when("/Menu2", {
-                    template : onmenu2(),
-                    controller: ctrlname
-                })
-                .otherwise({
-                    template : ondefault(),
-                    controller: ctrlname
-                });
-        }]);
-        otherapp.controller(ctrlname, ["$scope", function($scope) {
-            console.log("$scope = " + $scope.toString());
-        }]);
+        const app2 = new TestApp2();
+        app2.create(appname, ctrlname);
 
         angular.bootstrap(contener.getElementById(id), ['otherapp']);
 
@@ -112,11 +128,15 @@ class AngularTests {
 window.angularexe = () => {
     try {
         const local = new AngularTests();
-        local.testANGULAR1(document, "angutest1", "Firstname", "Lastname", ["Item 1", "Item 2", "Item 3"]);
+
+        local.testANGULAR1(document, "angutest1",
+                               "Firstname", "Lastname", ["Item 1", "Item 2", "Item 3"]);
+
         local.testANGULAR2(document, "angutest2");
     }
     catch (e) {
         console.log(e.toString())
+        alert("angular app failed")
     }
     finally {
         //...
